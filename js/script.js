937ee586b5786e39ec180744e4c03686cbe00b83
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-04-03 16:13:28 +0200

    [UPD] need a fix

--- js/script.js
+++ js/script.js
@@ -6,7 +6,21 @@ const endpoint="https://flynn.boolean.careers/exercises/api/random/mail"
 axios.get(endpoint).then(pippo => {
     //reuperare i valori generati
     const listaEmail = pippo.data.response;
-    // console.log(listaEmail);
-
-
+    console.log(listaEmail);
 })
+//mi dichiaro la variabile listaEmail vuota
+let listaEmail;
+
+//mi creo un ciclo for per fare 10 controlli della funzione axios che mi genera una email casuale.
+for(let i=0; i<10;i++){
+    axios.get(endpoint).then(pippo => {
+        //reuperare i valori generati
+        const listaEmail = pippo.data.response;
+        console.log(listaEmail);
+    })
+
+    inputEmail.innerHTML = listaEmail;
+}
+
+
+
